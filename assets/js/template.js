42a280a4e33dca20a183abb357ba330b3e2e9b6e
Stephen Fischer <sfischer@bidclerk.com>
2015-01-21 23:02:40 -0600

    registry

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,3 +1,18 @@
+$(function() {
+  $('#navbar a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 60
+        }, 1000);
+        return false;
+	  };
+	}
+  });
+});
+
 jQuery(document).ready(function($) {
 
 	var my_nav = $('.navbar-sticky'); 
